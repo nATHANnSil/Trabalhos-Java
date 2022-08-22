8b67443282a96e03f25431369ea611a3f9a4b629
Nathan <72050032+nATHANnSil@users.noreply.github.com>
2022-08-22 11:35:42 -0300

    Add files via upload
    soma

---
+++ 2022.2/introducao/src/introducao/Main.java
@@ -0,0 +1,18 @@
+package introducao;
+//
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**/ 
+		int intA = MyIO.readInt("Digite o primeiro numero:");
+		int intB = MyIO.readInt("Digite o segundo numero");
+		
+		int soma = intA+intB;
+		
+		MyIO.println(soma);
+
+	}
+
+}
